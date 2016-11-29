d4be03c5a704f7d5dc160bd468e501ff0984578a
davvalen <davidvalentinsemail@gmail.com>
2016-11-29 13:33:45 +0100

    bug fix : total agreement reset

--- docs/dist/pointing-poker.js
+++ docs/dist/pointing-poker.js
@@ -617,50 +617,6 @@
 
 (function() {
   'use strict';
-
-  /**
-   * @ngdoc directive
-   * @name pointingPoker.components:pointingPokerUserSelection
-   * @description
-   *
-   */
-
-  pointingPokerUserSelectionCtrl.$inject = ["$stateParams", "$state", "firebasePointingPokerService"];
-  angular.module('pointingPoker').component('pointingPokerUserSelection', {
-        bindings: {},
-        require: {
-          pointingPokerCtrl: '^pointingPoker'
-        },
-        controller: pointingPokerUserSelectionCtrl,
-        controllerAs: 'pointingPokerUserSelectionCtrl',
-        templateUrl: 'src/scripts/pointingPokerUserSelection/pointingPokerUserSelection.tpl.html'
-      }
-  );
-
-  /**
-   * @ngdoc controller
-   * @name pointingPoker.controllers:pointingPokerUserSelectionCtrl
-   * @description
-   *
-   */
-  function pointingPokerUserSelectionCtrl($stateParams,$state, firebasePointingPokerService) {
-
-    var self = this;
-    self.signIn = signIn;
-    
-    function signIn(userName) {
-      if(userName!='' && userName!=undefined){
-        $state.transitionTo('app.pointing', {roomNumber:  $stateParams.roomNumber, userName: userName});
-      }else{
-        alert('That can\'t be your name, don\'t lie to me. pls D:');
-      }
-    }
-
-  }
-})();
-
-(function() {
-  'use strict';
   angular.module('pointingPoker')
       .provider('firebasePointingPokerService', function() {
 
@@ -724,6 +680,8 @@
               user.vote = null;
             });
             room.votesShown = false;
+            room.someoneVoted = false;
+            room.totalAgreement=false;
             return room;
           }
 
@@ -787,3 +745,47 @@
       });
 })();
 
+
+(function() {
+  'use strict';
+
+  /**
+   * @ngdoc directive
+   * @name pointingPoker.components:pointingPokerUserSelection
+   * @description
+   *
+   */
+
+  pointingPokerUserSelectionCtrl.$inject = ["$stateParams", "$state", "firebasePointingPokerService"];
+  angular.module('pointingPoker').component('pointingPokerUserSelection', {
+        bindings: {},
+        require: {
+          pointingPokerCtrl: '^pointingPoker'
+        },
+        controller: pointingPokerUserSelectionCtrl,
+        controllerAs: 'pointingPokerUserSelectionCtrl',
+        templateUrl: 'src/scripts/pointingPokerUserSelection/pointingPokerUserSelection.tpl.html'
+      }
+  );
+
+  /**
+   * @ngdoc controller
+   * @name pointingPoker.controllers:pointingPokerUserSelectionCtrl
+   * @description
+   *
+   */
+  function pointingPokerUserSelectionCtrl($stateParams,$state, firebasePointingPokerService) {
+
+    var self = this;
+    self.signIn = signIn;
+    
+    function signIn(userName) {
+      if(userName!='' && userName!=undefined){
+        $state.transitionTo('app.pointing', {roomNumber:  $stateParams.roomNumber, userName: userName});
+      }else{
+        alert('That can\'t be your name, don\'t lie to me. pls D:');
+      }
+    }
+
+  }
+})();
